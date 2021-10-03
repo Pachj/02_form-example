575ae88a84050a6e4bf4412ca7d5cdaa1c86b8e4
Henry Joerg <henry.joerg@gmail.com>
2021-10-03 21:55:47 +0200

    project finished for the moment

--- src/PizzaOrder.jsx
+++ src/PizzaOrder.jsx
@@ -1,41 +1,66 @@
 import React, {useState} from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import logo from './images/pizza-image.jpeg';
 
 export default function PizzaOrder() {
-  const [order, setOrder] = useState('');
-  const [selectedIngredients, setSelectedIngredients] = useState('');
+  const [selectedIngredient1, setSelectedIngredient1] = useState('');
+  const [selectedIngredient2, setSelectedIngredient2] = useState('');
+  const [orderMessage, setOrderMessage] = useState('');
 
-  const ingredients = ['Schinken', 'Oliven', 'Ananas', 'Gorgonzola'];
-
-  const handleSelectChange = (event) => {
-    setSelectedIngredients(...selectedIngredients, event.target.value);
-  }
+  const ingredients = ['🥓 Speck', '🫒 Oliven', '🍍 Ananas', '🧀 Gorgonzola'];
 
   const orderPizza = () => {
+    let tmpArray = [];
 
+    if (selectedIngredient1 !== '') {
+      tmpArray.push(selectedIngredient1);
+    }
+    if (selectedIngredient2 !== '') {
+      tmpArray.push(selectedIngredient2);
+    }
+
+    const allSelectedIngredientsString = tmpArray.join(', ');
+
+    setOrderMessage(
+      <><p className={'full-width'}>Gewählte Zutaten: {allSelectedIngredientsString}</p>
+      <p className={'full-width bold'}>Deine Pizza ist unterwegs zu dir. &#127829;</p></>
+    )
   }
 
   return (
-    <div>
-      <h1>InJös Premium Pizza</h1>
-      <form onSubmit={orderPizza}>
-        <label>
-        <select value={selectedIngredients} onChange={handleSelectChange}>
+    <div className={'content-container'}>
+      <div className={'inner-content-container'}>
+        <img className={'logo'} src={logo} alt={'pizza'}/>
+      </div>
+      <div className={'inner-content-container'}>
+        <h1>InJös Premium Pizza</h1>
+      </div>
+      <div className={'inner-content-container'}>
+        <label className={'select-margin'}>
+          1. Zutat
+        <select className={'form-select form-select-sm'} value={selectedIngredient1} onChange={(event => {setSelectedIngredient1(event.target.value)})}>
+          <option value={''}>none</option>
           {ingredients.map((ingredient) => {
             return <option value={ingredient}>{ingredient}</option>
           })}
         </select>
-          Zutat
         </label>
-        <label>
-          <select value={selectedIngredients} onChange={handleSelectChange}>
+        <label className={'select-margin'}>
+          2. Zutat
+          <select className={'form-select form-select-sm'} value={selectedIngredient2} onChange={(event => {setSelectedIngredient2(event.target.value)})}>
+            <option value={''}>none</option>
             {ingredients.map((ingredient) => {
               return <option value={ingredient}>{ingredient}</option>
             })}
           </select>
-          Zutat
         </label>
-        <button type={'submit'}>Pizza bestellen</button>
-      </form>
+      </div>
+      <div className={'inner-content-container'}>
+        <button className={'btn btn-primary'} onClick={orderPizza}>Pizza bestellen</button>
+      </div>
+      <div className={'inner-content-container'}>
+        {orderMessage}
+      </div>
     </div>
   )
 }
